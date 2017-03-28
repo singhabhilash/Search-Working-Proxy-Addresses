aeb2c888c8a9d92d8e7024e33049f168344454fc
Abhilash Singh <daft300punk@gmail.com>
2017-03-28 15:49:46 +0530

    Use network js request with proxy

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,10 +2,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.css';
+import request from 'request';
 
 
 export default class Home extends Component {
+  startSearch() {
+    let baseUrl = 'http://172.16.116.';
+    for(let i = 0; i < 256; i++) {
+      let proxyUrl = baseUrl + i.toString() + ':3128';
+      console.log("Requesting " + proxyUrl);
+      var proxiedRequest = request.defaults({'proxy': proxyUrl});
+      proxiedRequest.get("http://google.com", function(err, resp, body) {
+        console.log('received back');
+        if(resp !== undefined && resp.statusCode === 200) console.log('working proxy', proxyUrl);
+      });
+    }
+  }
+
   render() {
+    this.startSearch();
     return (
       <div>
         <div className={styles.container} data-tid="container">
