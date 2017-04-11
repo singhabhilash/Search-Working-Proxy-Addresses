cd52a2de32024aa37cafcc86fd7447b6bec1973a
Abhilash Singh <daft300punk@gmail.com>
2017-04-11 21:32:32 +0530

    Non-async action creators tests complete

--- test/actions/index.spec.js
+++ test/actions/index.spec.js
@@ -11,4 +11,36 @@ describe('actions', () => {
     };
     expect(actions.requestSearch(searchType)).toEqual(expectedAction);
   });
+
+  it('should create create search started action', () => {
+    const expectedAction = {
+      type: actionTypes.SEARCH_STARTED,
+      isSearching: true
+    };
+    expect(actions.searchStarted()).toEqual(expectedAction);
+  });
+
+  it('should create searchFinished action', () => {
+    const expectedAction = {
+      type: actionTypes.SEARCH_FINISHED,
+      isSearching: false
+    };
+    expect(actions.searchFinished()).toEqual(expectedAction);
+  });
+
+  it('should create foundResult action', () => {
+    const workingUrl = 'http://0.0.0.0:3128';
+    const expectedAction = {
+      type: actionTypes.FOUND_RESULT,
+      result: workingUrl
+    };
+    expect(actions.foundResult(workingUrl)).toEqual(expectedAction);
+  });
+
+  it('should create stopSearching action', () => {
+    const expectedAction = {
+      type: actionTypes.STOP_SEARCHING,
+    };
+    expect(actions.stopSearching()).toEqual(expectedAction);
+  })
 });
